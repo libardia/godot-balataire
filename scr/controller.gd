fc380e53616a028ee8679f4f0111352358e19c11
Anthony Libardi <libardi42@gmail.com>
2025-06-19 00:28:01 -0400

    begin deal logic

--- scr/controller.gd
+++ scr/controller.gd
@@ -10,7 +10,8 @@ extends Node
 var piles: Array[Pile] = []
 var homes: Array[Pile] = []
 var can_draw: bool = true
-var setup_thread: Thread
+var accept_input: bool = false
+var deal_thread: Thread
 
 # ==================================================================================================
 
@@ -21,11 +22,12 @@ func _ready() -> void:
     for i in 4:
         var h := get_node("Layout/TopRow/Home%s" % i) as Pile
         homes.push_back(h)
-    setup_thread = Thread.new()
+    deal_thread = Thread.new()
+    deal_thread.start(deal)
 
 
 func _on_deck_selected() -> void:
-    if can_draw:
+    if can_draw and accept_input:
         can_draw = false
         if deck.cards.is_empty():
             # Reset cards
@@ -38,13 +40,18 @@ func _on_deck_selected() -> void:
             do_draw()
 
 
+func _on_stock_card_selected() -> void:
+    pass
+
+
+func deal():
+    for i in piles.size():
+        print(piles[i])
+
+
 func do_draw():
     var num_to_draw := mini(3, deck.cards.size())
     TimerHelper.delay_fire(num_to_draw * stock_deal_delay, func (): can_draw = true)
     # Make 3 (or less) timers to draw the cards
     for i in num_to_draw:
         TimerHelper.delay_fire(i * stock_deal_delay, deck.draw_n.bind(stock, 1, true))
-
-
-func _on_stock_card_selected() -> void:
-    pass
